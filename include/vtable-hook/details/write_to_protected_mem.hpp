8bfa2c4e2a66a50037f569f87e251c84996967f0
SanyaNya <sanyanya4ever@gmail.com>
2025-03-21 10:17:03 +0300

    write_to_protected_memory error handling and refactor

--- include/vtable-hook/details/write_to_protected_mem.hpp
+++ include/vtable-hook/details/write_to_protected_mem.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <system_error>
 
 #ifdef _WIN32 //Windows
 #include <Windows.h>
 #else //POSIX
+#include <cerrno>
 #include <unistd.h>
 #include <sys/mman.h>
 #endif
@@ -16,40 +18,33 @@ namespace {
 const std::uintptr_t pagesize = sysconf(_SC_PAGE_SIZE);
 #endif
 
-inline int unprotect_page(void* addr_in_page)
+inline int set_vtable_page_write_protection(void* addr_in_page, int orig_protection)
 {
 #ifdef _WIN32
   MEMORY_BASIC_INFORMATION mbi;
-  VirtualQuery(static_cast<LPCVOID>(addr_in_page), &mbi, sizeof(mbi));
-  VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_READWRITE, &mbi.Protect);
+  if(!VirtualQuery(static_cast<LPCVOID>(addr_in_page), &mbi, sizeof(mbi)))
+    throw std::system_error(::GetLastError(), std::system_category());
+
+  if(!VirtualProtect(mbi.BaseAddress, mbi.RegionSize, orig_protection ? orig_protection : PAGE_READWRITE, &mbi.Protect))
+    throw std::system_error(::GetLastError(), std::system_category());
+
   return mbi.Protect;
 #else
   void* region = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(addr_in_page) & ~(pagesize-1));
-  mprotect(region, pagesize, PROT_READ|PROT_WRITE|PROT_EXEC);
+  if(mprotect(region, pagesize, orig_protection ? orig_protection : PROT_READ|PROT_WRITE|PROT_EXEC) == -1)
+    throw std::system_error(errno, std::system_category());
   return PROT_READ|PROT_EXEC;
-#endif
-}
-
-inline void protect_page(void* addr_in_page, int orig_protection)
-{
-#ifdef WIN32
-  MEMORY_BASIC_INFORMATION mbi;
-  VirtualQuery(static_cast<LPCVOID>(addr_in_page), &mbi, sizeof(mbi));
-  VirtualProtect(mbi.BaseAddress, mbi.RegionSize, orig_protection, &mbi.Protect);
-#else
-  void* region = reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(addr_in_page) & ~(pagesize-1));
-  mprotect(region, pagesize, orig_protection);
 #endif
 }
 
 template<typename T>
 inline void write_to_protected_mem(T* addr, T value)
 {
-  int orig_protection = unprotect_page(static_cast<void*>(addr));
+  int orig_protection = set_vtable_page_write_protection(static_cast<void*>(addr), 0);
 
   *addr = value;
 
-  protect_page(static_cast<void*>(addr), orig_protection);
+  set_vtable_page_write_protection(static_cast<void*>(addr), orig_protection);
 }
 
 }} //namespace vthk::details::anonymous

639385a494925080d5c7ba87d7f9e2d226fc87fb
SanyaNya <sanyanya4ever@gmail.com>
2025-03-21 09:35:25 +0300

    Add VTHK_CALL calling convention macro

--- include/vtable-hook/vtable-hook.hpp
+++ include/vtable-hook/vtable-hook.hpp
@@ -11,19 +11,17 @@
 
 #if defined(_MSC_VER) //MSVC ABI
   #include "details/MSVC_VFuncPtr.hpp"
+  #define VTHK_CALL __fastcall
 #else //Itanium ABI
   #include "details/Itanium_VFuncPtr.hpp"
+  #define VTHK_CALL 
 #endif
 
 namespace vthk
 {
 
 template<typename T, typename R, typename... Args>
-#if defined(_MSC_VER) //MSVC ABI
-inline auto hook(const T* instance, R(T::*vfuncptr)(Args...), R(__fastcall *hook)(T*, Args...)) -> R(__fastcall *)(T*, Args...)
-#else //Itanium ABI
-inline auto hook(const T* instance, R(T::*vfuncptr)(Args...), R(*hook)(T*, Args...)) -> R(*)(T*, Args...)
-#endif
+inline auto hook(const T* instance, R(T::*vfuncptr)(Args...), R(VTHK_CALL *hook)(T*, Args...)) -> R(VTHK_CALL *)(T*, Args...)
 {
   static_assert(std::is_polymorphic_v<T>, "Class does not have virtual functions");
   details::VFuncPtr vfp(vfuncptr);
@@ -35,11 +33,7 @@ inline auto hook(const T* instance, R(T::*vfuncptr)(Args...), R(*hook)(T*, Args.
 
   details::write_to_protected_mem(&vtable[vfp.vtable_slot], std::bit_cast<std::uintptr_t>(hook));
 
-#if defined(_MSC_VER) //MSVC ABI
-  return std::bit_cast<R(__fastcall *)(T*, Args...)>(orig_vfuncptr);
-#else
-  return std::bit_cast<R(*)(T*, Args...)>(orig_vfuncptr);
-#endif
+  return std::bit_cast<R(VTHK_CALL *)(T*, Args...)>(orig_vfuncptr);
 }
 
 } //namespace vthk
